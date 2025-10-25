ebbe1b749b1cdad4bb4225c53c2d5e723e60f1c9
lauramun23 <lauramunoz.23@campuscamara.es>
2025-10-25 10:57:45 +0200

    Cambios Escenas

---
+++ Assets/Game/Scripts/ManagerMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ManagerMenu : MonoBehaviour
+{
+    public void CambioEscenaJuego()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void CambioEscenaTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void CambioEscenaCreditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void QuitarJuego()
+    {
+        Application.Quit();
+        Debug.Log("Se ha cerrado");
+    }
+
+
+}
